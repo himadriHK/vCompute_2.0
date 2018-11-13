237396eee481468295bbda76e3e0f56754e16a50
HimadriHK <himadri.hk@gmail.com>
2018-11-13 14:49:56 +0530

    Update _Server.cs

--- _Server.cs
+++ _Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -14,6 +14,8 @@ namespace ProjectCore
         public bool Stop { get; set; }
         private Dictionary<string, string> nodeList;
         private Dictionary<string, object> resultStore;
+        private Dictionary<string, _Common.Task> taskStore;
+	     private Dictionary<string, double> nodeStatus;
         private CodeLoader.Loader codeLoader;
         private int nodeCtr = 1;
         private int taskCtr = 1;
@@ -52,10 +54,10 @@ namespace ProjectCore
                 case REQUEST_CODES.REGISTER_ASSEMBLY:
                     RegisterAssembly(requestPacket, listCtxt.Response);
                     break;
-                case REQUEST_CODES.TASK:
+                case REQUEST_CODES.SUBMIT_TASK:
                     SubmitTask(requestPacket, listCtxt.Response);
                     break;
-                case REQUEST_CODES.RESULT:
+                case REQUEST_CODES.GET_RESULT:
                     SendResult(requestPacket, listCtxt.Response);
                     break;
                 case REQUEST_CODES.STATUS:
@@ -68,12 +70,6 @@ namespace ProjectCore
                     UnregisterAssembly(requestPacket, listCtxt.Response);
                     break;
             }
-            //DateTime dateTime = DateTime.Now;
-            //Thread.Sleep(100);
-            //DateTime dateTime2 = DateTime.Now;
-            //string str = "<html><body>Received at " + dateTime.ToString() + " Sent at " + dateTime2.ToString() + "</body></html>";
-            //listCtxt.Response.OutputStream.Write(Encoding.ASCII.GetBytes(str), 0, str.Length);
-            //listCtxt.Response.Close();
         }
 
         private void UnregisterAssembly(RequestPacket requestPacket, HttpListenerResponse response)
@@ -88,32 +84,113 @@ namespace ProjectCore
 
         private void UpdateStatus(RequestPacket requestPacket, HttpListenerResponse response)
         {
-            throw new NotImplementedException();
-        }
+	        bool errored = false;
+	        if (nodeList.ContainsValue(requestPacket.Node))
+	        {
+		        if (nodeStatus.ContainsKey(requestPacket.Node))
+		        {
+			        nodeStatus[requestPacket.Node] += ((requestPacket.Status.CpuTime + requestPacket.Status.RamUsage) / 2);
+		        }
+		        else
+		        {
+			        nodeStatus.Add(requestPacket.Node, ((requestPacket.Status.CpuTime + requestPacket.Status.RamUsage) / 2));
+		        }
+	        }
+	        else
+	        {
+		        errored = true;
+	        }
+
+	        ResponsePacket resPacket = new ResponsePacket()
+	        {
+		        DateTimeStamp = DateTime.Now,
+		        Code = RESPONSE_CODES.STATUS_ACK,
+		        Errored = errored
+	        };
+	        _Common.WriteResPacketToResponse(resPacket, response);
+		}
 
         private void SendResult(RequestPacket requestPacket, HttpListenerResponse response)
         {
-            throw new NotImplementedException();
-        }
+	        bool errored = false;
+	        object result = null;
+	        if (resultStore.ContainsKey(requestPacket.Task.TaskId))
+	        {
+		        result = resultStore[requestPacket.Task.TaskId];
+	        }
+	        else
+	        {
+		        errored = true;
+	        }
+	        ResponsePacket resPacket = new ResponsePacket()
+	        {
+		        DateTimeStamp = DateTime.Now,
+		        Code = RESPONSE_CODES.GET_RESULT_ACK,
+		        Errored = errored,
+				  Result = new Result() {ResultData = result,TaskId = requestPacket.Task.TaskId }
+	        };
+	        _Common.WriteResPacketToResponse(resPacket, response);
+		}
 
         private void SubmitTask(RequestPacket requestPacket, HttpListenerResponse response)
         {
-            throw new NotImplementedException();
-        }
+	        bool errored = false;
+	        if (nodeList.ContainsKey(requestPacket.Node))
+	        {
+		        taskStore.Add(requestPacket.Node,requestPacket.Task);
+	        }
+	        else
+	        {
+		        errored = true;
+	        }
+	        ResponsePacket resPacket = new ResponsePacket()
+	        {
+		        DateTimeStamp = DateTime.Now,
+		        Code = RESPONSE_CODES.SUBMIT_TASK_ACK,
+		        Errored = errored
+	        };
+	        _Common.WriteResPacketToResponse(resPacket, response);
+		}
 
         private void RegisterAssembly(RequestPacket requestPacket, HttpListenerResponse response)
         {
-            throw new NotImplementedException();
-        }
+	        bool errored = !codeLoader.codeDictionary.WriteAssembly(requestPacket.AssemblyDetails.AssemblyName,
+		        requestPacket.AssemblyDetails.AssemblyCode);
+
+		        ResponsePacket resPacket = new ResponsePacket()
+		        {
+			        DateTimeStamp = DateTime.Now,
+			        Code = RESPONSE_CODES.REGISTER_ASSEMBLY_ACK,
+			        Errored = errored
+		        };
+			_Common.WriteResPacketToResponse(resPacket, response);
+		}
 
         private void RegisterClient(HttpListenerRequest request, HttpListenerResponse response)
         {
-            if (!nodeList.ContainsKey(request.RemoteEndPoint.ToString()))
-            {
-                string newNodeName = "Node_" + nodeCtr++;
-                ResponsePacket resPacket = new ResponsePacket() { DateTimeStamp = DateTime.Now, Code = RESPONSE_CODES.REGISTER_CLIENT_ACK, Node = newNodeName };
-                _Common.WriteResPacketToResponse(resPacket,response);
-            }
+	        ResponsePacket resPacket;
+
+	        if (!nodeList.ContainsKey(request.RemoteEndPoint.ToString()))
+	        {
+		        string newNodeName = "Node_" + nodeCtr++;
+		        resPacket = new ResponsePacket()
+		        {
+			        DateTimeStamp = DateTime.Now,
+			        Code = RESPONSE_CODES.REGISTER_CLIENT_ACK,
+			        Node = newNodeName
+		        };
+	        }
+	        else
+	        {
+		        resPacket = new ResponsePacket()
+		        {
+			        DateTimeStamp = DateTime.Now,
+			        Code = RESPONSE_CODES.REGISTER_CLIENT_ACK,
+			        Errored = true
+		        };
+	        }
+	        _Common.WriteResPacketToResponse(resPacket, response);
+			}
         }
-    } //test
+    }
 }
